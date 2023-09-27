0e9cc6bf73ffb84f9abe2067353a63c000ba89be
Dat Nguyen <dathhcc2@gmail.com>
2023-09-27 10:07:42 +0700

    Oke

---
+++ src/hook/context/AppContext.js
@@ -0,0 +1,8 @@
+import { createContext, useContext, useState } from 'react';
+const AppContext = createContext();
+const AppProvider = (props) => {
+    const value = {};
+    return <AppContext.Provider value={value} {...props}></AppContext.Provider>;
+};
+
+export { AppProvider, AppContext };

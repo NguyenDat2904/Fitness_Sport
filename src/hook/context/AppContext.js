5f519238ccd3f0ce037c08a04ec44f65757351f3
Hung <vanhungnvh1712004@nvh1712004gmail.com>
2023-10-28 00:30:16 +0700

    admin

--- src/hook/context/AppContext.js
+++ src/hook/context/AppContext.js
@@ -1,6 +1,38 @@
 import { createContext, useEffect, useState } from 'react';
 const AppContext = createContext();
 const AppProvider = (props) => {
+    const [show, setShow] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [isCheckBlur, setIsCheckBlur] = useState(true);
+    const [statusCode, setStatusCode] = useState('');
+    const [dis, setDis] = useState('');
+    const [courses, setCourses] = useState([]);
+    const [userInfo, setUserInfo] = useState(localStorage.getItem('user'));
+    const [userParse, setUserParse] = useState(JSON.parse(localStorage.getItem('user')));
+
+    const onShow = () => {
+        if (show == '') {
+            setShow('show');
+        } else {
+            setShow('');
+        }
+    };
+
+    const onBlur = () => {
+        if (isCheckBlur) {
+            setShow('');
+        }
+    };
+
+    const onMouseEnter = () => {
+        // console.log(false);
+        setIsCheckBlur(false);
+    };
+
+    const onMouseLeave = () => {
+        // console.log(true);
+        setIsCheckBlur(true);
+    };
     // admin
     //client
     const [userData, setUser] = useState([]);
@@ -405,7 +437,27 @@ const [timePayment,setTimePayment]=useState("")
 
         //logOut
         idLogout,setIdLogOut,
-        statusLogout,setStatusLogout
+        statusLogout,setStatusLogout,
+        //
+        show: show,
+        loading: loading,
+        statusCode: statusCode,
+        dis: dis,
+        setShow,
+        setLoading,
+        onShow,
+        onBlur,
+        setIsCheckBlur,
+        onMouseEnter,
+        onMouseLeave,
+        setStatusCode,
+        setDis,
+        courses,
+        setCourses,
+        userInfo,
+        setUserInfo,
+        userParse,
+        setUserParse,
 
 
         
